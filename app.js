fcd8622ac76b79749555ce0c41567106d6eac1ad
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-06 21:25:35 -0300

    Ntalk

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-let express = require('express'), routes = require('./routes/home'), app = express();
+let express = require('express'), routes = require('./routes');
 
-app.set('views', __dirname + 'views');
+let app = express();
+
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', routes.index);
 app.get('/usuarios', routes.user.index);
 
-load('models').then('controllers').then('routes').into(app);
-
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log(`Ntalk no ar.`)
 })
-
-
