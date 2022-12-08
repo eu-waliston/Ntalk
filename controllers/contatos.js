1f5bfecc2979f4246cd328cffd77f3958730d359
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-08 09:07:26 -0300

    Ntalk

--- controllers/contatos.js
+++ controllers/contatos.js
@@ -1,9 +1,10 @@
-module.exports = function(app) {
+module.exports = function (app) {
     let ContatoController = {
-        index: function(req,res){
-            let usuario = req.session.usuario, params =  {usuario: usuario};
+        index: function (req, res) {
+            let usuario = req.session.usuario
+                , params = {usuario: usuario};
             res.render('contatos/index', params);
         }
     }
     return ContatoController;
-}
+};
--- controllers/home.js
+++ controllers/home.js
@@ -5,27 +5,22 @@ module.exports = function (app) {
         index: function (req, res) {
             res.render('home/index');
         },
-
         login: function (req, res) {
-            let email = req.body.usuario.email, nome = req.body.usuario.nome;
+            let email = req.body.usuario.email;
+            let nome = req.body.usuario.nome;
 
             if (email && nome) {
                 let usuario = req.body.usuario;
-                usuario['contatos'] = [];
-                req.session.usuario = usuario;
                 res.redirect('/contatos');
-
             } else {
                 res.redirect('/')
             }
-
         },
-
-        logout: function (req,res){
+        logout: function (req, res) {
            req.session.destroy();
            res.redirect('/');
         }
-
     };
+
     return HomeController;
-};
+}
