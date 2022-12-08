03e8e54c5b660bf7ce5285b2507d7a36c84b6542
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-08 08:40:19 -0300

    Ntalk

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 let express = require('express')
 let load = require('express-load')
+let cookieParser = require('cookie-parser')
+var session = require('express-session');
 
 app = express();
-
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.cookieParser('ntalk'));
-app.use(express.session());
+app.use(cookieParser('ntalk'));
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+
 app.use(express.json());
 app.use(express.urlencoded())
 app.use(express.static(__dirname + '/public'));
